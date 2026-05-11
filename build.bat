99429ab590bb3d17df2538c10c1e65a6f3e45f63
Juuso Piippo <juuso.piippo1@gmail.com>
2026-05-11 08:50:59 +0300

    Tried to get AddressSanitizer to be found automatically
    It's just a hassle with these tools... Got it workign by manually copying the dll to working dir though...

--- build.bat
+++ build.bat
@@ -20,10 +20,27 @@ rem not used: /Zc:__cplusplus
 set defines=-DCOMPRESSOR_WIN32=1
 set flags=/W4 /FC /Oi /EHa- /GR- /GS- /std:c++20 /nologo
 
+rem Couldn't get AddressSanitizer to be found automatically
+rem so just copied the clang_rt.asan_dynamic-x86_64.dll to root...
+
+rem set VS=""
+rem where /Q clssasdasdasd.exe || (
+rem     echo start
+rem   set __VSCMD_ARG_NO_LOGO=1
+rem   for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set VS=%%i
+rem   if "!VS!" == "" (
+rem     echo ERROR: Visual Studio installation not found
+rem     exit /b 1
+rem   )
+rem   echo calling VsDevCmd.bat
+rem   call "!VS!\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 -startdir=none -no_logo || exit /b 1
+rem )
+
 rem debug: /MTd /Zi /Od
 rem release: /MT /O2
 if !config! == debug (
     set defines=!defines! -DCOMPRESSOR_DEV=1 -DCOMPRESSOR_DEBUG=1
+    rem /fsanitize=address
     set flags=!flags! /MTd /Od /Zi
 ) else if !config! == release (
     set flags=!flags! /MT /O2
